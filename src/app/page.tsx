b1117a83546fe9ef5ba07979bbb529c120e6c52e
Stanislław Jakubowski <sjakubowski3554@gmail.com>
2024-07-26 12:34:47 +0200

    Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { UserModal } from "./user-modal";
+import { Suspense } from "react";
 
 const users = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 export default function Page() {
   return (
     <>
-      <UserModal />
+      <Suspense>
+        <UserModal />
+      </Suspense>
       <div className="flex h-screen flex-col items-center justify-center p-24 gap-2">
         {users.map((userId) => (
           <Button asChild key={userId}>
